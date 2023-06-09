9a8ad536271c2fb7bf672b18ba25f315f5aa7e7d
Ujjwal Upadhyay <90755156+iamujj15@users.noreply.github.com>
2023-06-09 23:56:04 +0530

    Add dynamic port for remote deployment

--- app.js
+++ app.js
@@ -63,6 +63,4 @@ app.post('/failure', function(req, res){
     res.redirect('/');
 });
 
-app.listen(3000, function(){
-    console.log('Server is running on port 3000');
-});
+app.listen(process.env.PORT || 3000);
